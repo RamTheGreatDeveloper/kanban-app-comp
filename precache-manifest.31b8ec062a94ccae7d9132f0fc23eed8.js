aa2d5c9241e0a3c87f926cdba25ff87e073ead46
Ram kumar <ramkumars.trichy@gmail.com>
2022-03-19 09:50:05 +0530

    Updates

---
+++ precache-manifest.31b8ec062a94ccae7d9132f0fc23eed8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8fc1d645137531d9c5de",
+    "url": "/kanban-app-comp/static/css/main.28f3d1df.chunk.css"
+  },
+  {
+    "revision": "8fc1d645137531d9c5de",
+    "url": "/kanban-app-comp/static/js/main.0f698e9c.chunk.js"
+  },
+  {
+    "revision": "f72957623757f0a2529e",
+    "url": "/kanban-app-comp/static/js/runtime~main.f3982ad7.js"
+  },
+  {
+    "revision": "48ee25ee70b34f3741c4",
+    "url": "/kanban-app-comp/static/js/2.356c579e.chunk.js"
+  },
+  {
+    "revision": "600236325b64565d5ca1736ca6589a16",
+    "url": "/kanban-app-comp/index.html"
+  }
+];
