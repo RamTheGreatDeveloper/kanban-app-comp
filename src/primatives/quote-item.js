3e0d2923a973cd2426cc8e27baf41058c550f56d
Ram kumar <ramkumars.trichy@gmail.com>
2022-03-23 10:36:23 +0530

    styled-componet-min

--- src/primatives/quote-item.js
+++ src/primatives/quote-item.js
@@ -28,97 +28,33 @@ const Container = styled.a`
   user-select: none;
   text-decoration: none;
   color: ${colors.N900};
-
   &:hover,
   &:active {
     color: ${colors.N900};
     text-decoration: none;
   }
-
   &:focus {
     outline: none;
     border-color: ${(props) => props.colors.hard};
     box-shadow: none;
   }
-
-  /* flexbox */
   display: flex;
   flex-direction: column;
 `;
 
-const Content = styled.div`
-  flex-grow: 1;
-  flex-basis: 100%;
-  display: flex;
-  flex-direction: column;
-`;
-
-const BlockQuote = styled.div`
-  color: rgb(165, 157, 157);
-  font-size: 12px;
-  margin-bottom: 5px;
-`;
-
-const Footer = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  font-size: 11px;
-  color: rgb(165, 157, 157);
-  font-weight: 600;
-`;
-
-const Date = styled.small`
-  flex-grow: 0;
-  margin: 0;
-  font-weight: bold;
-  color: #7e7472;
-  padding: 4px;
-`;
-const OrderNo = styled.div`
-  display: flex;
-  flex-direction: column;
-  font-size: 15px;
-  font-weight: 600;
-  margin-bottom: 5px;
-  color: rgb(177, 169, 169);
-`;
-const OrderTag = styled.div`
-  display: flex;
-`;
-const OrderId = styled.div`
-  font-size: 13px;
-  font-weight: 600;
-  color: #000;
-  margin-top: 5px;
-`;
+const Content = styled.div`flex-grow: 1;flex-basis: 100%;display: flex;flex-direction: column;`;
+const BlockQuote = styled.div`color: rgb(165, 157, 157);font-size: 12px;margin-bottom: 5px;`;
+const Footer = styled.div`display: flex;justify-content: space-between;align-items: center;font-size: 11px;color: rgb(165, 157, 157);font-weight: 600;`;
+const Date = styled.small`flex-grow: 0;margin: 0;font-weight: bold;color: #7e7472;padding: 4px;`;
+const OrderNo = styled.div`display: flex;flex-direction: column;font-size: 15px;font-weight: 600;margin-bottom: 5px;color: rgb(177, 169, 169);`;
+const OrderTag = styled.div`display: flex;`;
+const OrderId = styled.div`font-size: 13px;font-weight: 600;color: #000;margin-top: 5px;`;
 const Num = styled.div``;
-const Res = styled.div`
-  margin-left: 10px;
-  border: 1px solid rgb(94, 94, 231);
-  padding: 2px;
-  border-radius: 3px;
-  color: rgb(95, 95, 202);
-  background-color: #ece7e7;
-`;
-const Due = styled.span`
-  font-size: 12px;
-  font-weight: normal;
-`;
+const Res = styled.div`margin-left: 10px;border: 1px solid rgb(94, 94, 231);padding: 2px;border-radius: 3px;color: rgb(95, 95, 202);background-color: #ece7e7;`;
+const Due = styled.span`font-size: 12px;font-weight: normal;`;
 const DueDate = styled.div``;
-const Assign = styled.div`
-  display: flex;
-  align-items: center;
-  font-weight: normal;
-`;
-const AssignBy = styled.div`
-  width: 20px;
-  height: 20px;
-  border: 1px solid blue;
-  border-radius: 10px;
-  background-color: lightgray;
-  margin-left: 10px;
-`;
+const Assign = styled.div`display: flex;align-items: center;font-weight: normal;`;
+const AssignBy = styled.div`width: 20px;height: 20px;border: 1px solid blue;border-radius: 10px;background-color: lightgray;margin-left: 10px;`;
 export default class QuoteItem extends React.PureComponent {
   render() {
     const { quote, isDragging, isGroupedOver, provided } = this.props;
--- src/search.js
+++ src/search.js
@@ -1,27 +1,8 @@
 import React from "react";
 import "./styles.css";
 import styled from "styled-components";
-const StyledInput = styled.div`
-  i {
-    position: absolute;
-    left: 80.9%;
-    top: 42px;
-    padding: 9px 8px;
-    fill: black;
-    transition: 0.3s;
-  }
-`;
-const Input = styled.input`
-  height: 20px;
-  font-size: 14px;
-  border: none;
-  border-bottom: 1px solid #ebe5e5;
-  border-radius: 2px;
-  margin: 27px 8px 0px 594px;
-  padding: 8px;
-  outline: none;
-  box-sizing: border-box;
-`;
+const StyledInput = styled.div`i {position: absolute;left: 80.9%;top: 42px;padding: 9px 8px;fill: black;transition: 0.3s;}`;
+const Input = styled.input`height: 20px;font-size: 14px;border: none;border-bottom: 1px solid #ebe5e5;border-radius: 2px;margin: 27px 8px 0px 594px;padding: 8px;outline: none;box-sizing: border-box;`;
 const Table = styled.table``;
 const Tbody = styled.tbody``;
 const Tr = styled.tr``;
