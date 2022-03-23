d16af94f744eb91b239d56c402ba57ed854e2bb2
Ram kumar <ramkumars.trichy@gmail.com>
2022-03-23 15:32:39 +0530

    title-rmv

--- src/column.js
+++ src/column.js
@@ -3,7 +3,6 @@ import styled from "styled-components";
 import { colors } from "@atlaskit/theme";
 import QuoteList from "./primatives/quote-list";
 const Title =  styled.h4`padding: 8px;transition: background-color ease 0.2s;flex-grow: 1;user-select: none;position: relative;&:focus {outline: 2px solid ${colors.P100};outline-offset: 2px;}`;
-
 const Container = styled.div`margin: 8px;display: flex;flex-direction: column;background-color: #ebecf0;`;
 const Header = styled.div`display: flex;align-items: center;justify-content: center;border-top-left-radius: 2px;border-top-right-radius: 2px;font-size: 12px;transition: background-color 0.2s ease;&:hover {background-color: #f8f8f8;}`;
 export default class Column extends Component {
